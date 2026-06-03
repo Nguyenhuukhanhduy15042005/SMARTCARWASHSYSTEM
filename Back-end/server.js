c27d18bf13ea24fb8ba2efd644175f6504f65544
LeQuangThang <youremail@example.com>
2026-06-03 15:55:22 +0700

    Update

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,34 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-require('./db'); // Khởi tạo kết nối SQL Server ngay khi chạy ứng dụng
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./db"); // Khởi tạo kết nối SQL Server ngay khi chạy ứng dụng
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 // ========================================================
 // IMPORT ROUTERS (Mỗi thành viên sẽ viết code ở file route riêng)
 // ========================================================
-const authRouter = require('./routes/auth');       // Thắng: Đăng ký / Đăng nhập
-const userRouter = require('./routes/user');       // Duy: Quản lý Profile / Phân quyền
-const vehicleRouter = require('./routes/vehicle'); // Thái: Quản lý phương tiện
-const bookingRouter = require('./routes/booking'); // Trọng & Huy: Đặt lịch & Lịch sử & FSM
+const authRouter = require("./routes/auth"); // Thắng: Đăng ký / Đăng nhập
+const userRouter = require("./routes/user"); // Duy: Quản lý Profile / Phân quyền
+const vehicleRouter = require("./routes/vehicle"); // Thái: Quản lý phương tiện
+const bookingRouter = require("./routes/booking"); // Trọng & Huy: Đặt lịch & Lịch sử & FSM
 
 // ========================================================
-// MOUNT ROUTERS 
+// MOUNT ROUTERS
 // ========================================================
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
-app.use('/api/vehicles', vehicleRouter);
-app.use('/api/bookings', bookingRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/vehicles", vehicleRouter);
+app.use("/api/bookings", bookingRouter);
 
 // Test Endpoint
-app.get('/api/test', (req, res) => {
-    res.json({ message: "API Car Wash System hoạt động tốt!" });
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API Car Wash System hoạt động tốt!" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="1028138494317-46leqma1fm64vkoghpd343j4fre1o76d.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="1028138494317-jh8hgdm3310geha7bsdoul1g86oac7ec.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>,
 );
